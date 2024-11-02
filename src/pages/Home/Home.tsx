f415af7be57c11d36d9d0ea38e370f0e0b5b3f3b
Alisher <aliserb773@gmail.com>
2024-11-02 14:46:31 +0200

    added tailwind

---
+++ src/pages/Home/Home.tsx
@@ -0,0 +1,9 @@
+const Home = () => {
+    return (
+        <div className="container">
+            Home page
+        </div>
+    )
+}
+
+export default Home;
---
+++ src/pages/Note/Note.tsx
@@ -0,0 +1,9 @@
+const Note = () => {
+    return (
+        <div className="container">
+            Note page
+        </div>
+    )
+}
+
+export default Note;
