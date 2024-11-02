73be6858fa358c019ba99921b046a8c743dd2724
Alisher <aliserb773@gmail.com>
2024-11-02 14:45:26 +0200

    added Header&Footer

---
+++ src/components/layout/Header.tsx
@@ -0,0 +1,11 @@
+const Header = () => {
+    return (
+        <header className="header">
+            <h1 className="text-3xl font-bold underline">
+                Hello world!
+            </h1>
+        </header>
+    )
+}
+
+export default Header;
